c0131eadd1151f5e75b94d6eb41e278485e81e51
Gabriel Augusto <Gabriel.afg182@gmail.com>
2024-04-28 20:55:09 -0300

    feat: Documentation with Swagger UI

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,15 +1,20 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTaskDto {
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   description: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   date: Date;
 
   @IsNotEmpty()
+  @ApiProperty()
   duration: Date;
 }
